8ebc3a77963f6074d9120ba624b9344f2c8dcd55
Philipp Gühring <pg@futureware.at>
2023-08-02 11:10:33 +0200

    More comments for the code

--- Verilog/searchdefines.pl
+++ Verilog/searchdefines.pl
@@ -1,39 +1,45 @@
 #!/usr/bin/perl -w
 
+# Work through all *.sv and *.v files in the current directory
+my @files=(<*.sv>,<*.v>);
 
-foreach my $definefile(<*.sv>,<*.v>)
+my @search=(); # List for remembering all the defines that exist
+
+# First pass: Search for all defines:
+foreach my $filename(@files)
 {
-  #print "$definefile\n";
-  open IN,"<$definefile";
-  while(<IN>)
+  #print "$filename\n";
+  open IN,"<$filename"; # Opening a file
+  while(<IN>) # Reading every line
   {
-    if(m/\`define\s+([\w_]+)/)
+    if(m/\`define\s+([\w_]+)/) # Is it a define?
     {
-      #print "$definefile $1\n";
-      push @search,$1;
+      #print "$filename $1\n"; # Print what we found for debugging
+      push @search,$1; # Remember that we have to search for this define
     }
   }
-  close IN;
+  close IN; # Close the file again
 }
 
-foreach my $definefile(<*.sv>,<*.v>)
+# Second pass: Now search for the usages of the 
+foreach my $filename(@files)
 {
-  print "$definefile\n";
-  open IN,"<$definefile";
-  my $counter=1;
-  while(<IN>)
+  print "$filename\n"; # We print the filename, so that the user sees which files were processed
+  open IN,"<$filename"; # We open the file
+  my $linenumber=1; # Linenumber counter
+  while(<IN>) # We read each line
   {
-    s/\/\/.*$//;	  
-    foreach my $mysearch(@search)
+    s/\/\/.*$//; # We ignore any comments at the end of each line
+    foreach my $mysearch(@search) # We iterate over all the defines we have to search
     {
-      if(m/[^`]$mysearch/ && !m/\`define/)
+      if(m/[^`]$mysearch/ && !m/\`define/) # Is the define used here? Make sure it's not the original define line
       {
-        print "WARNING: $definefile:$counter $mysearch: $_";
+        print "WARNING: $filename:$linenumber $mysearch: $_"; # Print the warning
       }
     }
-    $counter++;
+    $linenumber++; # Linenumber counter
   }
-  close IN;
+  close IN; # Dont forget to close the files
 }
 
 
