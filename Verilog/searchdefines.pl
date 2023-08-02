00f90341ad13a99958e1ee03d8bfd7807e4facae
Philipp Gühring <pg@futureware.at>
2023-08-02 11:32:00 +0200

    Improved comments

--- Verilog/searchdefines.pl
+++ Verilog/searchdefines.pl
@@ -9,37 +9,37 @@ my @search=(); # List for remembering all the defines that exist
 foreach my $filename(@files)
 {
   #print "$filename\n";
-  open IN,"<$filename"; # Opening a file
+  open IN,"<$filename";
   while(<IN>) # Reading every line
   {
     if(m/\`define\s+([\w_]+)/) # Is it a define?
     {
-      #print "$filename $1\n"; # Print what we found for debugging
+      #print "$filename $1\n";
       push @search,$1; # Remember that we have to search for this define
     }
   }
-  close IN; # Close the file again
+  close IN;
 }
 
 # Second pass: Now search for the usages of the 
 foreach my $filename(@files)
 {
   print "$filename\n"; # We print the filename, so that the user sees which files were processed
-  open IN,"<$filename"; # We open the file
-  my $linenumber=1; # Linenumber counter
-  while(<IN>) # We read each line
+  open IN,"<$filename";
+  my $linenumber=1;
+  while(<IN>) # read each line
   {
-    s/\/\/.*$//; # We ignore any comments at the end of each line
-    foreach my $mysearch(@search) # We iterate over all the defines we have to search
+    s/\/\/.*$//; # Ignoring comments at the end of each line
+    foreach my $mysearch(@search) # iterate over all the defines we have to search
     {
-      if(m/[^`]$mysearch/ && !m/\`define/) # Is the define used here? Make sure it's not the original define line
+      if(m/[^`]$mysearch/ && !m/\`define/) # Is the define used here? Filter out the original `define line
       {
-        print "WARNING: $filename:$linenumber $mysearch: $_"; # Print the warning
+        print "WARNING: $filename:$linenumber $mysearch: $_";
       }
     }
-    $linenumber++; # Linenumber counter
+    $linenumber++;
   }
-  close IN; # Dont forget to close the files
+  close IN;
 }
 
 
